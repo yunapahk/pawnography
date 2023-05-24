5ad2006c0ceff7f0e061ba8e30c5987155d660dd
Yuna Pahk <yunapahk@gmail.com>
2023-05-24 16:50:05 -0700

    Starting chess board

--- pawnography.js
+++ pawnography.js
@@ -12,3 +12,15 @@ const settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
+
+var chessboard = document.getElementById('chessboard');
+   for (var i = 0; i < 8; i++) {
+      for (var j = 0; j < 8; j++) {
+         var chessSquare = document.createElement('div');
+         chessSquare.className = 'chess-square';
+         if ((i + j) % 2 == 0) {
+            chessSquare.style.backgroundColor = '#000';
+         }
+         chessboard.appendChild(chessSquare);
+      }
+    }
