624734a1027fd245f9ff9291ff7743031a0e2152
Yuna Pahk <yunapahk@gmail.com>
2023-05-25 15:58:58 -0700

    Notation completed

--- pawnography.js
+++ pawnography.js
@@ -1,7 +1,7 @@
 const settings = {
 	async: true,
 	crossDomain: true,
-	url: 'https://chess-puzzles2.p.rapidapi.com/random?number_of_puzzles=20',
+	url: 'https://chess-puzzles2.p.rapidapi.com/range?min=1200&max=1600&max_deviation=100',
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '02f3b69c35msh4a0aadf57595284p121757jsn8b3c7e60e543',
