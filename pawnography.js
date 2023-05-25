33df4b4ce47114f73a27907e2306d8a8db9c38ec
Yuna Pahk <yunapahk@gmail.com>
2023-05-25 00:09:09 -0700

    API you cost me  monies

--- pawnography.js
+++ pawnography.js
@@ -1,7 +1,7 @@
 const settings = {
 	async: true,
 	crossDomain: true,
-	url: 'https://chess-puzzles2.p.rapidapi.com/random?number_of_puzzles=30',
+	url: 'https://chess-puzzles2.p.rapidapi.com/random?number_of_puzzles=20',
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '02f3b69c35msh4a0aadf57595284p121757jsn8b3c7e60e543',
@@ -12,15 +12,3 @@ const settings = {
 $.ajax(settings).done(function (response) {
 	console.log(response);
 });
-
-var chessboard = document.getElementById('chessboard');
-   for (var i = 0; i < 8; i++) {
-      for (var j = 0; j < 8; j++) {
-         var chessSquare = document.createElement('div');
-         chessSquare.className = 'chess-square';
-         if ((i + j) % 2 == 0) {
-            chessSquare.style.backgroundColor = '#000';
-         }
-         chessboard.appendChild(chessSquare);
-      }
-    }
